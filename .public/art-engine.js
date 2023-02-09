90f53d50be75dcdaf8662fc3d12ece3566125e37
SaluteDan <da175@hotmail.co.uk>
2023-02-09 22:26:48 +0000

    clean up, offically v1.0

--- .public/art-engine.js
+++ .public/art-engine.js
@@ -1,7 +1,7 @@
-// //// ATTRIBUTES ART ENGINE --ALPHA v0.10
+// //// ATTRIBUTES ART ENGINE --ALPHA v0.15
 
-// // IMPORT -- Data From JSON (End Case will be Smart Contracts)
-// import collection from './JSON/data.json' assert {type: 'json'}
+// // // IMPORT -- Data From JSON (End Case will be Smart Contracts)
+// // import collection from './JSON/data.json' assert {type: 'json'}
 
 // // Get the canvas element
 // const canvas = document.getElementById("canvas");
@@ -12,96 +12,33 @@
 
 // const imageSources = initImageSources();
 
+// function mintArt() {
 
-// function generateArt(collection) {
+// // Get the staked amount for each option
+// const inputs = document.querySelectorAll("input[id^='attribute']");
+// for (const input of inputs) {
+//   const [attribute, option] = input.id.split("-");
+//   // access your object and update the input value
+//   collection.attributes[attribute][option].input = parseInt(input.value);
+// }
 
-
-//     // Create Map from Collection data
-
-//     // let attributes = {
-//     //   attribute1: {
-//     //     option1: {
-//     //       baseWeight: 100,
-//     //       stakedWeight: parseInt(attribute1Option1Input.value) || 0
-//     //     }, 
-//     //     option2: {
-//     //       baseWeight: 50,
-//     //       stakedWeight: parseInt(attribute1Option2Input.value) || 0
-//     //     },
-//     //     option3: {
-//     //       baseWeight: 10,
-//     //       stakedWeight: parseInt(attribute1Option3Input.value) || 0
-//     //     },
-//     //   },
-//     //   attribute2: {
-//     //     option1: {
-//     //       baseWeight: 10,
-//     //       stakedWeight: parseInt(attribute2Option1Input.value) || 0
-//     //     },
-//     //     option2: {
-//     //       baseWeight: 120,
-//     //       stakedWeight: parseInt(attribute2Option2Input.value) || 0
-//     //     },
-//     //     option3: {
-//     //       baseWeight: 150,
-//     //       stakedWeight: parseInt(attribute2Option3Input.value) || 0
-//     //     },
-//     //   },
-//     //   attribute3: {
-//     //     option1: {
-//     //       baseWeight: 60,
-//     //       stakedWeight: parseInt(attribute3Option1Input.value) || 0
-//     //     },
-//     //     option2: {
-//     //       baseWeight: 100,
-//     //       stakedWeight: parseInt(attribute3Option2Input.value) || 0
-//     //     },
-//     //     option3: {
-//     //       baseWeight: 100,
-//     //       stakedWeight: parseInt(attribute3Option3Input.value) || 0
-//     //     },
-//     //   }
-//     // };
-//     //
-//     // let attribute1Option1Input = document.getElementById("attribute1-option1");
-//     // let attribute1Option2Input = document.getElementById("attribute1-option2");
-//     // let attribute1Option3Input = document.getElementById("attribute1-option3");
-
-//     // let attribute2Option1Input = document.getElementById("attribute2-option1");
-//     // let attribute2Option2Input = document.getElementById("attribute2-option2");
-//     // let attribute2Option3Input = document.getElementById("attribute2-option3");
-
-//     // let attribute3Option1Input = document.getElementById("attribute3-option1");
-//     // let attribute3Option2Input = document.getElementById("attribute3-option2");
-//     // let attribute3Option3Input = document.getElementById("attribute3-option3");
-
-
-//   // Get the staked amount for each option
-//   const inputs = document.querySelectorAll("input[id^='attribute']");
-//   for (const input of inputs) {
-//     const [attribute, option] = input.id.split("-");
-//     // access your object and update the input value
-//     collection.attributes[attribute][option].input = parseInt(input.value);
-    
-//     // return collection
-//   }
-
-// console.log(collection)
-
-// // Define a function to get the selected option for each attribute
+// // FUNCTION -- Define a function to get the selected option for each attribute
 // function getSelectedOption(attribute) {
 //   // Total weight of all options
 //   let totalWeight = 0;
 //   // Map of all options and their weights
 //   const optionMap = {};
 //   // Loop through options in attribute
+//   let inputValues = [];
 //   for (let option in attribute) {
-//     if (option !== "name" && option !== "Weight" && option !== "cost" && option !== "selected") {
-//       const optionWeight = attribute[option].Weight + (attribute[option].input || 0);
-//       optionMap[option] = optionWeight;
-//       totalWeight += optionWeight;
-//     }
-//   }
+//       if (option !== "name" && option !== "Weight" && option !== "cost" && option !== "selected") {
+//           const optionWeight = attribute[option].Weight + (attribute[option].input || 0);
+//           optionMap[option] = optionWeight;
+//           totalWeight += optionWeight;
+//           inputValues.push(attribute[option].input);
+//       }
+//   console.log(inputValues)
+//   }  
 //   // Select a random option based on weight
 //   let random = Math.floor(Math.random() * totalWeight);
 //   for (let option in optionMap) {
@@ -127,9 +64,6 @@
 // let imageSource2 = imageSources["attribute2"][selectedAttribute2Option];
 // let imageSource3 = imageSources["attribute3"][selectedAttribute3Option];
   
-  
-  
-
 //   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 //   let image3Promise = new Promise((resolve, reject) => {
@@ -164,79 +98,54 @@
 //     ctx.drawImage(images[2], 0, 0, canvas.width, canvas.height);
 //   });
 
-//   // function getSelectedWeight(attribute) {
-//   //   // Total weight of all options
-//   //   let totalWeight = 0;
-//   //   // Map of all options and their weights
-//   //   const optionMap = {};
-//   //   // Loop through options in attribute
-//   //   for (let option in attributes[attribute]) {
-//   //     // Add option's weight to total weight
-//   //     totalWeight += attributes[attribute][option];
-//   //   }
-//   //   return totalWeight;
-//   // }
+//   // Push the selected option into the collection object
+//   collection.attributes.attribute1.selected = selectedAttribute1Option;
+//   collection.attributes.attribute2.selected = selectedAttribute2Option;
+//   collection.attributes.attribute3.selected = selectedAttribute3Option;
 
-//   // // get the total weight for each attribute
-//   // let weight1 = getSelectedWeight("attribute1");
-//   // let weight2 = getSelectedWeight("attribute2");
-//   // let weight3 = getSelectedWeight("attribute3");
+//   console.log(collection)
 
-//   // update the front-end with selected options 
-//   document.getElementById("attribute1meta").innerHTML = `<small>ATTRIBUTE 1: ${selectedAttribute1Option}</small>`;
-//   document.getElementById("attribute2meta").innerHTML = `<small>ATTRIBUTE 2: ${selectedAttribute2Option}</small>`;
-//   document.getElementById("attribute3meta").innerHTML = `<small>ATTRIBUTE 3: ${selectedAttribute3Option}</small>`;
-  
-//   // // update the front-end with the weight values
-//   // document.getElementById("attribute1Weight").innerHTML = `<p> You staked ${weight1} coins on Attribute 1</p>`;
-//   // document.getElementById("attribute2Weight").innerHTML = `<p> You staked ${weight2} coins on Attribute 2</p>`;
-//   // document.getElementById("attribute3Weight").innerHTML = `<p> You staked ${weight3} coins on Attribute 3</p>`;
-
-//   // let "option1" = selectedAttribute1Option;
-//   // let "option2" = selectedAttribute2Option;
-//   // let "option3" = selectedAttribute2Option;
-
-//   const metadata = [{selectedAttribute1Option, selectedAttribute2Option, selectedAttribute3Option}];
-
-//   const attribute1selector = document.getElementById("attribute1")
-//   const attribute2selector = document.getElementById("attribute2")
-//   const attribute3selector = document.getElementById("attribute3")
-
-//   let activeAttribute1Option = attribute1selector.getElementsByClassName(selectedAttribute1Option)[0];
-//   let activeAttribute2Option = attribute2selector.getElementsByClassName(selectedAttribute2Option)[0];
-//   let activeAttribute3Option = attribute3selector.getElementsByClassName(selectedAttribute3Option)[0];
-
-//   activeAttribute1Option.classList.add("selected");
-//   activeAttribute2Option.classList.add("selected");
-//   activeAttribute3Option.classList.add("selected");
-
+//   const attribute1selector = document.querySelectorAll("[attribute='attribute1'] span");
+//   attribute1selector.forEach(function(element) {
+//       if (element.classList.contains(selectedAttribute1Option)) {
+//           element.classList.add("active");
+//           element.classList.add("show");
+//       } else {
+//           element.classList.remove("active");
+//           element.classList.remove("show");
+//       }
+//   })
+//   const attribute2selector = document.querySelectorAll("[attribute='attribute2'] span");
+//   attribute2selector.forEach(function(element) {
+//       if (element.classList.contains(selectedAttribute2Option)) {
+//           element.classList.add("active");
+//           element.classList.add("show");
+//       } else {
+//           element.classList.remove("active");
+//           element.classList.remove("show");
+//       }
+//   })
+//   const attribute3selector = document.querySelectorAll("[attribute='attribute3'] span");
+//   attribute3selector.forEach(function(element) {
+//       if (element.classList.contains(selectedAttribute3Option)) {
+//           element.classList.add("active");
+//           element.classList.add("show");
+//       } else {
+//           element.classList.remove("active");
+//           element.classList.remove("show");
+//       }
+//   })    
 //   // Return the selected options
-//   console.log(metadata)
+//   console.log(canvas.toDataURL());
 
-//   return metadata;
+//   const link = document.createElement('a');
+
+//   link.download = 'download.png';
+  
+//   link.href = canvas.toDataURL();
+//   link.click();
+//   link.delete;
+ 
 //   }
 
-
-//   function initImageSources() {
-//   const imageSources = {
-//     attribute1: {
-//       option1: "imgs/C01/1/1.png",
-//       option2: "imgs/C01/1/2.png",
-//       option3: "imgs/C01/1/3.png"
-//     },
-//     attribute2: {
-//       option1: "imgs/C01/2/1.png",
-//       option2: "imgs/C01/2/2.png",
-//       option3: "imgs/C01/2/3.png"
-//     },
-//     attribute3: {
-//       option1: "imgs/C01/3/1.png",
-//       option2: "imgs/C01/3/2.png",
-//       option3: "imgs/C01/3/3.png"
-//     }
-//   };
-//   console.log("Image sources:", imageSources);
-//   return imageSources;
-// }
-
-// window.onload = generateArt(collection);
+//   export {canvas, ctx};
--- .public/init.js
+++ .public/init.js
@@ -1,8 +1,10 @@
-"use strict";
+// Load the FileSaver.js library
 
 // IMPORT -- Data From JSON (End Case will be Smart Contracts)
 import collection from './JSON/data.json' assert {type: 'json'}
 import Membership from './JSON/member.json' assert {type: 'json'}
+
+
 
 // import generateArt from './art-engine.js'
 
@@ -11,8 +13,9 @@ function generateLabels(attributes) {
   let inputLabels = '';
   for (let attribute in attributes) {
     inputLabels += `<nav id="${attribute}" class="navigation" aria-label="Page navigation example">
-                        <span class="attribute-option">${attributes[attribute]['name']} 
-                          <small>ATTR: ${attributes[attribute]["cost"]}</small>
+                        <span class="attribute-option">
+                          <span>${attributes[attribute]['name']}<small>ATTR: ${attributes[attribute]["cost"]}</small></span>
+                          <span class="${attribute}-active badge bg-secondary float-end "></span>
                         </span>
                         <div class="d-flex align-items-start justify-content-between"><ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">`;
     for (let option in attributes[attribute]) {
@@ -26,23 +29,28 @@ function generateLabels(attributes) {
     for (let option in attributes[attribute]) {
       if (option !== "name" && option !== "Weight" && option !== "cost" && option !== "selected") {
         inputLabels += `<span class="number-input tab-pane ${option}" attribute="${attribute}" id="${attribute}-${option}-pane" role="tabpanel" aria-labelledby="${attribute}-${option}" tabindex="0">
+        <button type="button" class="btn stake-focus opacity-50 btn-outline-secondary">STAKE</button>
+        <div class="wrapper d-none" attribute="${attribute}">
         <button class="input-increment btn btn-outline-secondary btn-sm" input="stepdown" option="${attribute}-${option}" type="button">&laquo;</button>
           <input id="${attribute}-${option}" type="number" value="0" min="0">
         <button class="input-increment btn btn-outline-secondary btn-sm" input="stepup" option="${attribute}-${option}" type="button">&raquo;</button>
+        </div>
       </span>`
       }
     }
     inputLabels +=`</div></nav>`;
   }
   return inputLabels;
-  // <small id="${attribute}-input" class="btn btn-secondary btn-sm">Small button</small>
 }
 
-document.querySelector("#attribute1-option2")
-
 // INIT -- Frontend with collection data
-let navContainer = document.getElementById("nav-wrapper");
+
+const navContainer = document.getElementById("nav-wrapper");
+
 navContainer.innerHTML = generateLabels(collection.attributes);
+
+
+
 // INIT -- the front-end with collection data 
 const collectionlabel = document.getElementsByClassName("collection-name")
 for (var i = 0; i < collectionlabel.length; i++) {
@@ -53,24 +61,44 @@ for (var i = 0; i < costlabel.length; i++) {
   costlabel[i].innerHTML = `${collection.mint.cost}`;
 }
 // update the front-end with member data 
-document.getElementById("coin-balance").innerHTML = `BALANCE ${Membership.balance}`;
+document.getElementById("coin-balance").innerHTML = `BALANCE ${Membership.balance}`
 
 
-// LISTENERS --  
+// Update the innerHTML of the element that displays the total sum
+  const stakedlabel = document.getElementsByClassName("points-staked")[0];
+  const pointsafterlabel = document.getElementsByClassName("points-after")[0];
+
+// Calculate the final balance after removing the mint fee and sum of active inputs
+const finalBalance = Membership.balance - collection.mint.cost
+
+pointsafterlabel.innerHTML = finalBalance;
+
+const myModal = document.getElementById('stake-modal')
+
+// CLICK LISTENERS --  
 document.addEventListener("click", function(e){
+
 // DRAW BUTTON --
-const draw = e.target.closest('#redraw');
-  if (draw) {
-    generateArt();
-  }
+  const draw = e.target.closest('#confirm-mint');
+    if (draw) {
+      generateArt()
+    } 
+    
+// STAKE BUTTON --
+  const stake = e.target.closest('#nav-wrapper  .stake-focus');
+    if (stake) {
+      // Used ID of parent Node to target button and wrapper
+      let id = stake.parentNode.id;
+      let wrapper = document.getElementById(id).querySelectorAll('div')[0];
+
+      stake.classList.toggle("d-none");
+      wrapper.classList.toggle("d-none");
+    }
 
 // MINT BUTTON -- 
-  const mint = e.target.closest('#mint');
+  const mint = e.target.closest('#mint-modal');
   if(mint){
-    const myModal = document.getElementById('stake-modal');
-    myModal.addEventListener('shown.bs.modal', () => {
-      mint.onclick()
-    })
+    myModal.addEventListener('shown.bs.modal', () => {})
   }
 // ATTRIBUTE OPTIONS --
   const option = e.target.closest('.nav-link');
@@ -100,21 +128,45 @@ const draw = e.target.closest('#redraw');
       let input = target.getAttribute("input");
       let attribute = target.parentNode.getAttribute("attribute");
       let cost = collection.attributes[attribute]["cost"]
-      let optionValue = document.getElementById(option).value;
+      let optionValue = parseInt(document.getElementById(option).value);
 
       console.log(option, input); 
       if(input === 'stepdown'){
-          if(optionValue - 10 >= 0)
-              document.getElementById(option).value = parseInt(optionValue) - cost;
+          if(optionValue - cost >= 0) {
+              document.getElementById(option).value = optionValue - cost;
+          } else {
+              document.getElementById(option).value = 0;
+              // Hide the div if the input value is 0 or less
+              let div = document.getElementsByClassName(`${attribute}-active`)[0];
+              div.style.display = "none";    
+          }
       }
       else if(input === 'stepup'){
-          document.getElementById(option).value = parseInt(optionValue) + cost;
+          document.getElementById(option).value = optionValue + cost;
+          // Update the innerHTML of the div
+          let div = document.getElementsByClassName(`${attribute}-active`)[0];
+          div.style.display = "block";
       }
-    return
-  }
-});
 
-//// ATTRIBUTES ART ENGINE --ALPHA v0.11
+      let updatedOptionValue = parseInt(document.getElementById(option).value);
+      let div = document.getElementsByClassName(`${attribute}-active`)[0];
+      div.innerHTML = `ATTR. COST ${updatedOptionValue}`;
+
+      // Calculate the sum of all the active inputs
+      const activeInputs = navContainer.querySelectorAll('.active input');
+      let sum = 0;
+      for (const input of activeInputs) {
+        sum += parseInt(input.value, 10);
+      }
+
+      stakedlabel.innerHTML = sum;
+      pointsafterlabel.innerHTML = finalBalance;
+
+      return;
+  }
+})
+
+//// ATTRIBUTES ART ENGINE --ALPHA v0.15
 
 // // IMPORT -- Data From JSON (End Case will be Smart Contracts)
 // import collection from './JSON/data.json' assert {type: 'json'}
@@ -180,9 +232,6 @@ let imageSource1 = imageSources["attribute1"][selectedAttribute1Option];
 let imageSource2 = imageSources["attribute2"][selectedAttribute2Option];
 let imageSource3 = imageSources["attribute3"][selectedAttribute3Option];
   
-  
-  
-
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   let image3Promise = new Promise((resolve, reject) => {
@@ -190,6 +239,9 @@ let imageSource3 = imageSources["attribute3"][selectedAttribute3Option];
     image3.src = imageSource3;
     image3.onload = () => {
       resolve(image3);
+    };
+    image3.onerror = (error) => {
+      reject(error);
     };
   });
   
@@ -199,6 +251,9 @@ let imageSource3 = imageSources["attribute3"][selectedAttribute3Option];
     image2.onload = () => {
       resolve(image2);
     };
+    image2.onerror = (error) => {
+      reject(error);
+    };
   });
   
   let image1Promise = new Promise((resolve, reject) => {
@@ -207,38 +262,38 @@ let imageSource3 = imageSources["attribute3"][selectedAttribute3Option];
     image1.onload = () => {
       resolve(image1);
     };
+    image1.onerror = (error) => {
+      reject(error);
+    };
   });
   
   Promise.all([image3Promise, image2Promise, image1Promise]).then((images) => {
     ctx.drawImage(images[0], 0, 0, canvas.width, canvas.height);
+    console.log(images["0"]);
     ctx.globalCompositeOperation = 'destination-over';
     ctx.drawImage(images[1], 0, 0, canvas.width, canvas.height);
+    console.log(images["1"]);
     ctx.globalCompositeOperation = 'destination-over';
     ctx.drawImage(images[2], 0, 0, canvas.width, canvas.height);
+    console.log(images["2"]);
+
+    //
+    //
+    // Saving canvas to file type 
+    //
+    // Convert the canvas to a blob
+    console.log(canvas.toDataURL("image/png"));
+
+    const link = document.createElement('a');
+
+    link.download = 'download.png';
+    link.href = canvas.toDataURL();
+    link.click();
+    
+    // canvas.toBlob(function(blob) {
+    //   saveAs(blob, "pretty image.png");
+    // }, "image/png");
   });
-
-  // function getSelectedWeight(attribute) {
-  //   // Total weight of all options
-  //   let totalWeight = 0;
-  //   // Map of all options and their weights
-  //   const optionMap = {};
-  //   // Loop through options in attribute
-  //   for (let option in attributes[attribute]) {
-  //     // Add option's weight to total weight
-  //     totalWeight += attributes[attribute][option];
-  //   }
-  //   return totalWeight;
-  // }
-
-  // // get the total weight for each attribute
-  // let weight1 = getSelectedWeight("attribute1");
-  // let weight2 = getSelectedWeight("attribute2");
-  // let weight3 = getSelectedWeight("attribute3");
-
-  // // update the front-end with the weight values
-  // document.getElementById("attribute1Weight").innerHTML = `<p> You staked ${weight1} coins on Attribute 1</p>`;
-  // document.getElementById("attribute2Weight").innerHTML = `<p> You staked ${weight2} coins on Attribute 2</p>`;
-  // document.getElementById("attribute3Weight").innerHTML = `<p> You staked ${weight3} coins on Attribute 3</p>`;
 
   // Push the selected option into the collection object
   collection.attributes.attribute1.selected = selectedAttribute1Option;
@@ -276,9 +331,10 @@ let imageSource3 = imageSources["attribute3"][selectedAttribute3Option];
           element.classList.remove("active");
           element.classList.remove("show");
       }
-  })    
-  // Return the selected options
-  }
+  })
+
+  
+}
 
   // FUNCTION -- to get the user selected option for each attribute and draw live (so user can see what the option looks like)
   function drawselected() {
@@ -372,10 +428,8 @@ let imageSource3 = imageSources["attribute3"][selectedAttribute3Option];
   function addSelectedClass(attribute, selectedAttributeOption){
     const attributeSelector = document.querySelectorAll("[attribute='" + attribute + "']." + selectedAttributeOption);
     attributeSelector.forEach(element => element.classList.add("selected"));
-    }
-    
-  // Return the selected options
-    }  
+  }
+}  
 
 function initImageSources() {
   const imageSources = {
@@ -398,7 +452,130 @@ function initImageSources() {
   console.log("Image sources:", imageSources);
   return imageSources;
 }
+function Artinit() {
+
+  // Get the staked amount for each option
+  const inputs = document.querySelectorAll("input[id^='attribute']");
+  for (const input of inputs) {
+    const [attribute, option] = input.id.split("-");
+    // access your object and update the input value
+    collection.attributes[attribute][option].input = parseInt(input.value);
+  }
+  
+  // FUNCTION -- Define a function to get the selected option for each attribute
+  function getSelectedOption(attribute) {
+    // Total weight of all options
+    let totalWeight = 0;
+    // Map of all options and their weights
+    const optionMap = {};
+    // Loop through options in attribute
+    let inputValues = [];
+    for (let option in attribute) {
+        if (option !== "name" && option !== "Weight" && option !== "cost" && option !== "selected") {
+            const optionWeight = attribute[option].Weight + (attribute[option].input || 0);
+            optionMap[option] = optionWeight;
+            totalWeight += optionWeight;
+            inputValues.push(attribute[option].input);
+        }
+    console.log(inputValues)
+    }  
+    // Select a random option based on weight
+    let random = Math.floor(Math.random() * totalWeight);
+    for (let option in optionMap) {
+      random -= optionMap[option];
+      if (random < 0) {
+        return option;
+      }
+    }
+  }
+  
+  const attributes = collection.attributes;
+  let selectedAttribute1Option = getSelectedOption(attributes.attribute1);
+  let selectedAttribute2Option = getSelectedOption(attributes.attribute2);
+  let selectedAttribute3Option = getSelectedOption(attributes.attribute3);
+  
+    
+  console.log("Selected option for attribute 1: ", selectedAttribute1Option);
+  console.log("Selected option for attribute 2: ", selectedAttribute2Option);
+  console.log("Selected option for attribute 3: ", selectedAttribute3Option);
+  
+  // Get the image source for each selected option
+  let imageSource1 = imageSources["attribute1"][selectedAttribute1Option];
+  let imageSource2 = imageSources["attribute2"][selectedAttribute2Option];
+  let imageSource3 = imageSources["attribute3"][selectedAttribute3Option];
+    
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+    let image3Promise = new Promise((resolve, reject) => {
+      let image3 = new Image();
+      image3.src = imageSource3;
+      image3.onload = () => {
+        resolve(image3);
+      };
+    });
+    
+    let image2Promise = new Promise((resolve, reject) => {
+      let image2 = new Image();
+      image2.src = imageSource2;
+      image2.onload = () => {
+        resolve(image2);
+      };
+    });
+    
+    let image1Promise = new Promise((resolve, reject) => {
+      let image1 = new Image();
+      image1.src = imageSource1;
+      image1.onload = () => {
+        resolve(image1);
+      };
+    });
+    
+    Promise.all([image3Promise, image2Promise, image1Promise]).then((images) => {
+      ctx.drawImage(images[0], 0, 0, canvas.width, canvas.height);
+      ctx.globalCompositeOperation = 'destination-over';
+      ctx.drawImage(images[1], 0, 0, canvas.width, canvas.height);
+      ctx.globalCompositeOperation = 'destination-over';
+      ctx.drawImage(images[2], 0, 0, canvas.width, canvas.height);
+    });
+  
+    // Push the selected option into the collection object
+    collection.attributes.attribute1.selected = selectedAttribute1Option;
+    collection.attributes.attribute2.selected = selectedAttribute2Option;
+    collection.attributes.attribute3.selected = selectedAttribute3Option;
+  
+    console.log(collection)
+  
+    const attribute1selector = document.querySelectorAll("[attribute='attribute1'] span");
+    attribute1selector.forEach(function(element) {
+        if (element.classList.contains(selectedAttribute1Option)) {
+            element.classList.add("active");
+            element.classList.add("show");
+        } else {
+            element.classList.remove("active");
+            element.classList.remove("show");
+        }
+    })
+    const attribute2selector = document.querySelectorAll("[attribute='attribute2'] span");
+    attribute2selector.forEach(function(element) {
+        if (element.classList.contains(selectedAttribute2Option)) {
+            element.classList.add("active");
+            element.classList.add("show");
+        } else {
+            element.classList.remove("active");
+            element.classList.remove("show");
+        }
+    })
+    const attribute3selector = document.querySelectorAll("[attribute='attribute3'] span");
+    attribute3selector.forEach(function(element) {
+        if (element.classList.contains(selectedAttribute3Option)) {
+            element.classList.add("active");
+            element.classList.add("show");
+        } else {
+            element.classList.remove("active");
+            element.classList.remove("show");
+        }
+    }) 
+  }
 
 
-
-window.onload = generateArt(collection);
+window.onload = Artinit(collection);
